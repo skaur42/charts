c9e4ea0021c1a19283174d916700709b2e0a2bb2
Simran Kaur <74691300+skaur42@users.noreply.github.com>
2023-03-21 01:52:53 -0400

    Update lineChart.js

--- lineChart.js
+++ lineChart.js
@@ -3,3 +3,5 @@
 // Must provide: dataPoints object, canvas id, legend id
 // Each data point is a name and an array of numeric values (e.g. name:[123,234,345])
 //=======================================================================================================
+
+
