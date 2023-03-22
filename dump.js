4e9c1ae3dd273be4c1fc68d2d99a2037fbf87102
Simran Kaur <74691300+skaur42@users.noreply.github.com>
2023-03-21 20:25:00 -0400

    Create dump.js

---
+++ dump.js
@@ -0,0 +1,8 @@
+//==========================================================================================
+// dump: This function takes any variable (primitive, array, hashes, object), 
+//       and will return a text representation of that object content.
+//       This function is similar to PHP print_r function
+//      args: variable - any variable - primitive, array, hash(associative array), object
+//            level    - (optional)
+//    return: The textual representation of the array.
+//==========================================================================================
